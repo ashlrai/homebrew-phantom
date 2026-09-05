70c4442e7adbb1aeb4db2a99d4835ed95e48798d
Mas <masonwyatt2003@gmail.com>
2026-09-05 04:38:21 -0400

    chore: publish Phantom v0.7.8 (#6)
    Co-authored-by: Mason Wyatt <masonwyatt23@users.noreply.github.com>

--- Formula/phantom.rb
+++ Formula/phantom.rb
@@ -17,23 +17,23 @@ class Phantom < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/ashlrai/phantom-secrets/releases/download/v0.7.7/phantom-aarch64-apple-darwin.tar.gz"
-      sha256 "24dbfc995d07fc27380c24dcd26a86418bc907ed29e0242a31144a3b542ddbaf"
+      url "https://github.com/ashlrai/phantom-secrets/releases/download/v0.7.8/phantom-aarch64-apple-darwin.tar.gz"
+      sha256 "ee13e62defab60ee39224d627943b288c8b1c1361e0e577f39038fbfdae7f6fc"
     end
     on_intel do
-      url "https://github.com/ashlrai/phantom-secrets/releases/download/v0.7.7/phantom-x86_64-apple-darwin.tar.gz"
-      sha256 "4df03729e60613e79debbbe6472e448034c882bd956afb63b7d3acb5788b1ad7"
+      url "https://github.com/ashlrai/phantom-secrets/releases/download/v0.7.8/phantom-x86_64-apple-darwin.tar.gz"
+      sha256 "5aad11ee1a26ff9c83f60df6ced7e81191f636c9d2d80451f664ca95f145e711"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ashlrai/phantom-secrets/releases/download/v0.7.7/phantom-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "cda0a895f8427a1602cf286daed7071097c2342408f751e4fdcf35437c4be2e3"
+      url "https://github.com/ashlrai/phantom-secrets/releases/download/v0.7.8/phantom-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "4383311bfb7f80219af6fcfd164fec88a021b174ed4c2c9267bf15350511349b"
     end
     on_intel do
-      url "https://github.com/ashlrai/phantom-secrets/releases/download/v0.7.7/phantom-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "399740ddf2226d18ce6458150ae4faae10febb63c386b8439000912b8ea8a64b"
+      url "https://github.com/ashlrai/phantom-secrets/releases/download/v0.7.8/phantom-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "9c7ca625fed3b54bd866526074986e7f41c271879d81ce7fe532bb3887787d77"
     end
   end
 
